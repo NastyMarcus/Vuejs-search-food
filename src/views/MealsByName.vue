216518f5945575a888ff54a0927738431381bc5d
MarcusShen <killthefullmoon@gmail.com>
2023-04-01 04:08:46 -0400

    Search for meals

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -1,10 +1,38 @@
 <template>
-    <div>
-        By Name
+    <div class="p-8">
+        <input
+            type="text" 
+            v-model="keyword"
+            class="rounded border-2 border-gray-200 w-full"
+            placeholder="Search for Meals"
+            @change="searchMeals"
+        />
+    </div>
+
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
+        <div v-for="meal of meals" :key="meal.idMeal" class="bg-white shadow">
+            <img :src="meal.strMealThumb" :alt="strMeal">
+            <h3>{{ meal.strMeal }}</h3>
+            <div>
+                <a :href="meal.strYoutube" target="_blank">Youtube</a>
+                <router-link to="/">
+                    View
+                </router-link>
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup>
+import { computed, ref } from 'vue';
+import store from '../store';
 
+const keyword = ref('');
+const meals = computed(() => store.state.searchedMeals);
+
+
+function searchMeals() {
+    store.dispatch('searchMeals', keyword.value)
+}
 
 </script>
