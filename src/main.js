f4122c756c8355c6f8f07185e804c47aef404a34
MarcusShen <killthefullmoon@gmail.com>
2023-04-01 01:58:34 -0400

    Create-Homepage

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
-import './style.css'
+import router from './router'
+import store from './store'
 import App from './App.vue'
+import './style.css'
+import "./assets/css/tailwind.css"
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(router)
+    .use(store)
+    .mount('#app')
